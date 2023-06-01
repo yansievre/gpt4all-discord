1e5d0cc8feef6963c5e9604794881b6fad479ad8
YANSILEGION\yansi <yansi.evre@kolibrigames.com>
2023-06-01 20:42:32 +0200

    changed index and added bin

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Eris = require("eris");
 const GPT4 = require('./gpt.js');
 
-let token = "";
+let token = "MTExMzg4NTE5NTEwMDIyNTU5Nw.G-ntDt.fdCQKUiq3lFs0gSYWK96uOA-oNGsluyU_APVYo";
 
-let GPT = new GPT4('./gpt4all-lora-quantized-win64');
+//let GPT = new GPT4('./gpt4all-lora-quantized-win64');
+let GPT = new GPT4('./ai/gpt4all-lora-quantized-linux-x86'); //Linux
 
 GPT.on('ready', async () => {
 	new Bot(token).start();
